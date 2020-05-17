e7293755820a531de17c8cf2ee6656b34a9744ed
haluk <haluktiras@gmail.com>
2020-05-17 16:54:28 +0300

    Mockito - creating mock inline and annotation

---
+++ src/test/java/com/ht/InlineMockTest.java
@@ -0,0 +1,18 @@
+package com.ht;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+
+public class InlineMockTest {
+
+    @Test
+    void testInlineMocks() {
+        Map mapMock = mock(Map.class);
+
+        assertEquals(mapMock.size(), 0);
+    }
+}
---
+++ src/test/java/com/ht/petclinic/AnnotationMocksTest.java
@@ -0,0 +1,24 @@
+package com.ht.petclinic;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.Map;
+
+public class AnnotationMocksTest {
+
+    @Mock
+    Map<String, Object> mapMock;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    void testMcok() {
+        mapMock.put("keyValue", "foo");
+    }
+}
